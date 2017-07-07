ba7b574ada89fefc9bd0458cec7064bb172d9789
Joakim Kemeny <joakim@kemeny.se>
2017-07-07 13:03:37 +0200

    Cleanup

--- Scripts/turnVpnOff.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env osascript
-
-tell application "System Events"
-    tell current location of network preferences
-        set VPNservice to service "Kemeny" -- name of the VPN service
-        disconnect VPNservice
-    end tell
-end tell
--- setup.sh
+++ setup.sh
@@ -7,16 +7,6 @@ if [[ -e ~/.oh-my-customizations ]]; then
 fi
 ln -s ${SRC_DIR}/.oh-my-customizations ~/.oh-my-customizations
 
-if [[ -e ~/.planter ]]; then
-	rm ~/.planter
-fi
-ln -s ${SRC_DIR}/.planter ~/.planter
-
-if [[ -e ~/Scripts ]]; then
-	rm ~/Scripts
-fi
-ln -s ${SRC_DIR}/Scripts ~/Scripts
-
 ln -sf ${SRC_DIR}/.ssh/config ~/.ssh/config
 ln -sf ${SRC_DIR}/.gitconfig ~/.gitconfig
 ln -sf ${SRC_DIR}/.gitignore_global ~/.gitignore_global
