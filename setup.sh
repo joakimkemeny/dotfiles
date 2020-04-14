f430ebd9ad71fd5537db04fe494c5de4ce4bd4b6
Joakim Kemeny <joakim@kemeny.se>
2020-04-14 23:23:55 +0200

    Tweaks

--- setup.sh
+++ setup.sh
@@ -11,5 +11,6 @@ ln -sf ${SRC_DIR}/.ssh/config ~/.ssh/config
 ln -sf ${SRC_DIR}/.gitconfig ~/.gitconfig
 ln -sf ${SRC_DIR}/.gitignore_global ~/.gitignore_global
 ln -sf ${SRC_DIR}/.hushlogin ~/.hushlogin
+ln -sf ${SRC_DIR}/.prettierrc ~/Projects/.prettierrc
 ln -sf ${SRC_DIR}/.vimrc ~/.vimrc
 ln -sf ${SRC_DIR}/.zshrc ~/.zshrc
